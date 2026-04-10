ec14f4625ccbc72136061deeac3866e9dc948462
Lyn Smith <lynsmith5770@gmail.com>
2026-04-09 19:29:43 -0700

    variables

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("-- Variable --");
+
+// String (text) ""
+let firstName = "Samantha";
+// numerical variables
+let myAge = 99;
+// boolean (true/false)
+let isStudent = false;
+let isProfessor = true;
+// display the values
+console.log(firstName);
+console.log(myAge);
+console.log(isStudent);
+console.log(isProfessor);
+
+// Mini challenge:
+// last name
+let lastName = "Smith";
+// height
+let height = 71;
+// display 
+console.log(lastName);
+console.log(height + " in");
